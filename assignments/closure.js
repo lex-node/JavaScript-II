8c166cff3c3e7080b8d5208f3ae69bd0b6b21776
lex_node <gabriel.j.shapiro@gmail.com>
2019-05-23 10:24:02 -0400

    solves Challenge-1 in closure.js

--- assignments/closure.js
+++ assignments/closure.js
@@ -1,5 +1,10 @@
 // ==== Challenge 1: Write your own closure ====
 // Write a simple closure of your own creation.  Keep it simple!
+let villain = "Verbal Kint";
+let villainDeclarer = () => console.log(`I am ${villain}`);
+villainDeclarer();
+villain = "Keyser Soze";
+villainDeclarer();
 
 
 /* STRETCH PROBLEMS, Do not attempt until you have completed all previous tasks for today's project files */
