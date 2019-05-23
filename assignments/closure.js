e5c74f905208bd76e2de744bb34c38395ccb578a
lex_node <gabriel.j.shapiro@gmail.com>
2019-05-23 13:43:04 -0400

    solves Challenge-2 (stretch) in closure.js

--- assignments/closure.js
+++ assignments/closure.js
@@ -12,11 +12,18 @@ villainDeclarer();
 
 // ==== Challenge 2: Create a counter function ====
 const counter = () => {
-  // Return a function that when invoked increments and returns a counter variable.
+    let count = 0;
+    return () => {
+        return count++;
+    }
 };
-// Example usage: const newCounter = counter();
-// newCounter(); // 1
-// newCounter(); // 2
+
+const newCounter = counter();
+
+console.log(newCounter());
+console.log(newCounter());
+
+
 
 // ==== Challenge 3: Create a counter function with an object that can increment and decrement ====
 const counterFactory = () => {
