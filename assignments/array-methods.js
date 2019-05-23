1e43163a06235b199d24cf12ba0b3640a45931d7
lex_node <gabriel.j.shapiro@gmail.com>
2019-05-23 09:23:18 -0400

    solves challenge-4 in array-methods.js

--- assignments/array-methods.js
+++ assignments/array-methods.js
@@ -62,9 +62,8 @@ console.log(fullName);
 
 // ==== Challenge 2: Use .map() ====
 // The event director needs to have all the runner's first names converted to uppercase because the director BECAME DRUNK WITH POWER. Convert each first name into all caps and log the result
-let allCaps = [];
 
-allCaps = runners.map(runner => {
+let allCaps = runners.map(runner => {
     var newRunner = {};
     return newRunner.first_name = runner.first_name.toUpperCase();
 });
@@ -75,21 +74,26 @@ console.log(allCaps);
 // ==== Challenge 3: Use .filter() ====
 // The large shirts won't be available for the event due to an ordering issue.  Get a list of runners with large sized shirts so they can choose a different size.
 // Return an array named largeShirts that contains information about the runners that have a shirt size of L and log the result
-let largeShirts = [];
 
-largeShirts = runners.filter(runner => {return runner.shirt_size === "L"});
+let largeShirts = runners.filter(runner => {return runner.shirt_size === "L"});
 
 console.log(largeShirts);
 
 // ==== Challenge 4: Use .reduce() ====
 // The donations need to be tallied up and reported for tax purposes. Add up all the donations into a ticketPriceTotal array and log the result
-let ticketPriceTotal = [];
-console.log(ticketPriceTotal);
+
+let ticketPriceTotal = runners.reduce((ticketPriceSum, runner ) => ticketPriceSum + runner.donation, 0);
+
+console.log("$" + ticketPriceTotal);
+
 
 // ==== Challenge 5: Be Creative ====
-// Now that you have used .forEach(), .map(), .filter(), and .reduce().  I want you to think of potential problems you could solve given the data set and the 5k fun run theme.  Try to create and then solve 3 unique problems using one or many of the array methods listed above.
+// Now that you have used .forEach(), .map(), .filter(), and .reduce().  I want you to think of potential problems you could solve given the data set and the 5k fun run theme.
+// Try to create and then solve 3 unique problems using one or many of the array methods listed above.
 
 // Problem 1
+//It turns out that some of the runners are actually dogs: Gussy, Fidel and Dollie. Please add for each runner whether the runner is a human or a dog.
+
 
 // Problem 2
 
