a0285bd1e70bdfed52ad2d9816a998701ca0d960
lex_node <gabriel.j.shapiro@gmail.com>
2019-05-22 22:23:12 -0400

    solves callbacks.js problems (except for stretch problems)

--- assignments/callbacks.js
+++ assignments/callbacks.js
@@ -1,4 +1,5 @@
-// Create a higher order function and invoke the callback function to test your work. You have been provided an example of a problem and a solution to see how this works with our items array.  Study both the problem and the solution to figure out the rest of the problems.
+// Create a higher order function and invoke the callback function to test your work. You have been provided an example of a problem and a solution to see how this works with our items array.
+// Study both the problem and the solution to figure out the rest of the problems.
 
 const items = ['Pencil', 'Notebook', 'yo-yo', 'Gum'];
 
@@ -27,24 +28,54 @@ const items = ['Pencil', 'Notebook', 'yo-yo', 'Gum'];
 
 function getLength(arr, cb) {
   // getLength passes the length of the array into the callback.
+    return cb(arr.length);
 }
+
+getLength(items, (length) => {
+    console.log(length);
+})
 
 function last(arr, cb) {
   // last passes the last item of the array into the callback.
-}
+    return cb(arr[arr.length-1]);
+};
+
+last(items, (lastElement) => {
+    console.log(lastElement);
+})
 
 function sumNums(x, y, cb) {
   // sumNums adds two numbers (x, y) and passes the result to the callback.
+  return cb(x+y);
 }
+
+sumNums(1, 2, (sum) =>{
+    console.log(sum);
+});
 
 function multiplyNums(x, y, cb) {
   // multiplyNums multiplies two numbers and passes the result to the callback.
+    return cb(x*y);
 }
+
+multiplyNums(2, 2, (product) =>{
+    console.log(product);
+});
 
 function contains(item, list, cb) {
   // contains checks if an item is present inside of the given array/list.
   // Pass true to the callback if it is, otherwise pass false.
+    return cb(list.includes(item));
 }
+
+contains('Pencil', items, (boolValue) => {
+    console.log(boolValue);
+})
+
+contains('Pickle', items, (boolValue) => {
+    console.log(boolValue);
+})
+
 
 /* STRETCH PROBLEM */
 
