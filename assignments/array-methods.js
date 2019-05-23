a587214662e375d54ba4ac91b7b93deaafc7611c
lex_node <gabriel.j.shapiro@gmail.com>
2019-05-23 08:22:47 -0400

    solves challenge-2 in array-methods.js

--- assignments/array-methods.js
+++ assignments/array-methods.js
@@ -63,7 +63,11 @@ console.log(fullName);
 // ==== Challenge 2: Use .map() ====
 // The event director needs to have all the runner's first names converted to uppercase because the director BECAME DRUNK WITH POWER. Convert each first name into all caps and log the result
 let allCaps = [];
-allCaps = runners.map(runner => {runner.first_name = (runner.first_name).toUpperCase()});
+
+allCaps = runners.map(runner => {
+    var newRunner = {};
+    return newRunner.first_name = runner.first_name.toUpperCase();
+});
 console.log(allCaps);
 
 
